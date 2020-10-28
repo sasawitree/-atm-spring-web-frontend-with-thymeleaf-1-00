1d4a56c9bca66ef75ed789b1c71a9b9f67add87c
sasawitree <72645124+sasawitree@users.noreply.github.com>
2020-10-28 12:10:24 +0700

    Add files via upload

---
+++ atm/atm/src/main/java/th/ac/kmitl/atm/controller/HomeController.java
@@ -0,0 +1,21 @@
+package th.ac.kmitl.atm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+public class HomeController {
+
+    public static Class<?> java;
+
+    @RequestMapping("/home")
+    public String getHomePage(Model model) {
+        model.addAttribute("greeting", "Sawaddee");
+        // return home.html
+        return "home";
+    }
+}
+
+
+
